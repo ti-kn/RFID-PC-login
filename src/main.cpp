e2b8eed16b23e89387094648d1f8da4d47c93539
Timotej Knezevic <timiknezi@icloud.com>
2026-06-26 16:28:41 +0200

    deleted matrix function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include "LedControl.h"
 #include <SPI.h>
 #include <MFRC522.h>
 
@@ -7,52 +6,8 @@ const byte BUTTON_PIN = 6;
 const byte SDA_PIN = 10;
 const byte RST_PIN = 9;
 
-LedControl matrix = LedControl(2, 4, 3, 1); // Pins: DIN, CLK, CS, number of modules
 MFRC522 rfidReader(SDA_PIN, RST_PIN);
 
-const byte arrowRight[] = 
-{
-  0b00000000,
-  0b00011000, 
-  0b00011000, 
-  0b00011000, 
-  0b01111110, 
-  0b00111100, 
-  0b00011000, 
-  0b00000000
-};
-
-const byte confirmed[] =
-{
-  0b00000000, 
-  0b00010000, 
-  0b00100110, 
-  0b01000000, 
-  0b01000000, 
-  0b00100110, 
-  0b00010000, 
-  0b00000000
-};
-
-const byte denied[] =
-{
-  0b00000000,
-  0b01000000, 
-  0b00100110, 
-  0b00010000, 
-  0b00010000, 
-  0b00100110, 
-  0b01000000, 
-  0b00000000
-};
-
-void DrawAtMatrix(const byte data[])
-{
-  for (int pixelCount = 0; pixelCount < 8; pixelCount++)
-  {
-    matrix.setRow(0, pixelCount, data[pixelCount]);
-  }
-}
 
 String masterID;
 void setup() 
@@ -60,12 +15,6 @@ void setup()
   Serial.begin(9600);
   SPI.begin();
   rfidReader.PCD_Init();
-
-  matrix.shutdown(0, false);      
-  matrix.setIntensity(0, 1);       
-  matrix.clearDisplay(0);
-
-  DrawAtMatrix(arrowRight);
 
   Serial.println("Karte einlesen");
   while (true)
@@ -106,20 +55,17 @@ void loop()
 
   if (idToCheck == masterID)
   {
-    DrawAtMatrix(confirmed);
     Serial.print("ID: ");
     Serial.println(idToCheck);
   } 
   else 
   {
-    DrawAtMatrix(denied);
     Serial.print("ID: ");
     Serial.println(idToCheck);
   }
   rfidReader.PICC_HaltA();
 
   delay(2000);
-  DrawAtMatrix(arrowRight);
 }
   
 
